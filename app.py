9be7b9da1aa5258f7b338cda4f4baacc0fab6841
Chevady <35552608+sherlock666@users.noreply.github.com>
2020-05-26 22:53:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,42 +7,44 @@ from flask import jsonify
 from flask import request
 from flask import Response
 
-# 取得webhook post訊息
-data = json.loads(request.get_data())
-# 需要的input在data['text']欄位中，並使用regex來正規化 
-text = re.sub('\<at\>.*\</at\>','',data['text'])
 
-r = requests.get(f'https://westus.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/349a200c-2e5d-41ff-9fc7-cac6fe9ab941/slots/production/predict?subscription-key=b70645171382440cbaf63ad0f332d077&verbose=false&show-all-intents=true&log=true&query='+text,headers=headers, params=params)
-raw=r.json()
-#print(data['query'])
-#print(data['prediction'])
-print(raw['prediction']['topIntent'])
-topIntent=raw['prediction']['topIntent']
-print(raw['prediction']['intents'][topIntent]['score'])
 
-#data['prediction']['intents']['全部成績']['score']
-
-if topIntent == '成績詢問' :
-    answer = "請問您想詢問什麼成績?"
-
-if topIntent == '全部成績' :
-    answer = "馬上幫您列出所有成績?"
-
-if topIntent == '國文成績' :
-    answer = "馬上幫您列出國文成績"
-
-if topIntent == '數學成績' :
-    answer = "馬上幫您列出數學成績"
-
-if topIntent == '英文成績' :
-    answer = "馬上幫您列出英文成績"
 
 
 app = Flask(__name__)
 
 @app.route("/" , methods=['POST'])
 def hello():
+    # 取得webhook post訊息
+    data = json.loads(request.get_data())
+    # 需要的input在data['text']欄位中，並使用regex來正規化 
+    text = re.sub('\<at\>.*\</at\>','',data['text'])
+####
+    r = requests.get(f'https://westus.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/349a200c-2e5d-41ff-9fc7-cac6fe9ab941/slots/production/predict?subscription-key=b70645171382440cbaf63ad0f332d077&verbose=false&show-all-intents=true&log=true&query='+text,headers=headers, params=params)
+    raw=r.json()
+    #print(data['query'])
+    #print(data['prediction'])
+    print(raw['prediction']['topIntent'])
+    topIntent=raw['prediction']['topIntent']
+    print(raw['prediction']['intents'][topIntent]['score'])
 
+    #data['prediction']['intents']['全部成績']['score']
+
+    if topIntent == '成績詢問' :
+        answer = "請問您想詢問什麼成績?"
+
+    if topIntent == '全部成績' :
+        answer = "馬上幫您列出所有成績?"
+
+    if topIntent == '國文成績' :
+        answer = "馬上幫您列出國文成績"
+
+    if topIntent == '數學成績' :
+        answer = "馬上幫您列出數學成績"
+
+    if topIntent == '英文成績' :
+        answer = "馬上幫您列出英文成績"
+####
     # get response by chatbot() funciton
     response = answer
     # response json至Teams webhook 
