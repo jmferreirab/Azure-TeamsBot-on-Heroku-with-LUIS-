91e8abf9e8a2abff1370680d62c200f700142fc0
Chevady <35552608+sherlock666@users.noreply.github.com>
2020-05-26 22:50:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,18 @@ from flask import jsonify
 from flask import request
 from flask import Response
 
+# 取得webhook post訊息
+data = json.loads(request.get_data())
+# 需要的input在data['text']欄位中，並使用regex來正規化 
+text = re.sub('\<at\>.*\</at\>','',data['text'])
+
 r = requests.get(f'https://westus.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/349a200c-2e5d-41ff-9fc7-cac6fe9ab941/slots/production/predict?subscription-key=b70645171382440cbaf63ad0f332d077&verbose=false&show-all-intents=true&log=true&query='+text,headers=headers, params=params)
-data=r.json()
+raw=r.json()
 #print(data['query'])
 #print(data['prediction'])
-print(data['prediction']['topIntent'])
-topIntent=data['prediction']['topIntent']
-print(data['prediction']['intents'][topIntent]['score'])
+print(raw['prediction']['topIntent'])
+topIntent=raw['prediction']['topIntent']
+print(raw['prediction']['intents'][topIntent]['score'])
 
 #data['prediction']['intents']['全部成績']['score']
 
@@ -37,10 +42,7 @@ app = Flask(__name__)
 
 @app.route("/" , methods=['POST'])
 def hello():
-    # 取得webhook post訊息
-    data = json.loads(request.get_data())
-    # 需要的input在data['text']欄位中，並使用regex來正規化 
-    text = re.sub('\<at\>.*\</at\>','',data['text'])
+
     # get response by chatbot() funciton
     response = answer
     # response json至Teams webhook 
