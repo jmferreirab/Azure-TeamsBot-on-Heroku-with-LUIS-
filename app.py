cddd2997c922d7f7d8eb227e964400e14ae74146
Chevady <35552608+sherlock666@users.noreply.github.com>
2020-05-26 22:33:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,21 @@ print(data['prediction']['intents'][topIntent]['score'])
 
 #data['prediction']['intents']['全部成績']['score']
 
+if topIntent == '成績詢問' :
+    message = "請問您想詢問什麼成績?"
+    return 0 
+if topIntent == '全部成績' :
+    message = "馬上幫您列出所有成績?"
+    return 0 
+if topIntent == '國文成績' :
+    message = "馬上幫您列出國文成績"
+    return 0 
+if topIntent == '數學成績' :
+    message = "馬上幫您列出數學成績"
+    return 0 
+if topIntent == '英文成績' :
+    message = "馬上幫您列出英文成績"
+    return 0 
 
 app = Flask(__name__)
 
@@ -27,7 +42,7 @@ def hello():
     # 需要的input在data['text']欄位中，並使用regex來正規化 
     text = re.sub('\<at\>.*\</at\>','',data['text'])
     # get response by chatbot() funciton
-    response = chatbot(text)
+    response = answer
     # response json至Teams webhook 
     message =jsonify( {"type": "message", # type: 回覆型態
                        "text": response}) # text: 回覆內容
